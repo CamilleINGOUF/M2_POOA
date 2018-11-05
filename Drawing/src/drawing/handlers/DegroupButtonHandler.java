8ac2cb54fa69eec7d34551a1fad5f8327f39377a
CamilleINGOUF <ingouf.camille@gmail.com>
2018-11-05 16:22:57 +0100

    degroup groups

--- Drawing/src/drawing/handlers/DegroupButtonHandler.java
+++ Drawing/src/drawing/handlers/DegroupButtonHandler.java
@@ -1,19 +1,41 @@
 package drawing.handlers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import drawing.shapes.IShape;
+import drawing.shapes.ShapeGroup;
 import drawing.ui.DrawingPane;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 public class DegroupButtonHandler implements EventHandler<ActionEvent> {
 
+	private DrawingPane dPane;
+	
 	public DegroupButtonHandler(DrawingPane drawingPane) {
-		// TODO Auto-generated constructor stub
+		this.dPane = drawingPane;
 	}
 
 	@Override
 	public void handle(ActionEvent event) {
-		// TODO Auto-generated method stub
+		List<IShape> selectedShapes = dPane.getSelection();
+		List<ShapeGroup> groups = new ArrayList<>();
+		for(IShape shape : selectedShapes) {
+			if(shape instanceof ShapeGroup) {
+				groups.add((ShapeGroup)shape);
+			}
+		}
 		
+		for(ShapeGroup g : groups)
+			degroup(g);
+	}
+	
+	private void degroup(ShapeGroup group) {
+		List<IShape> shapes = group.getShapes();
+		dPane.removeShape(group);
+		for(IShape s : shapes)
+			dPane.addShape(s);
 	}
 
 }
